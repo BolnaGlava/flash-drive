e3958a8645ab4de86f3b00e34333075f9ac588fb
Bolna Glava <sezerrr10@abv.bg>
2025-11-23 21:54:43 +0200

    ╨Р╨▓╤В╨╛ нед 23.11.2025 г. 21:54:43,59

---
+++ pizzas.py
@@ -0,0 +1,17 @@
+magicians = ['gandalf','vezuvi','lord zul']
+for magican in magicians:
+    print(f"{magican.title()} that was a great magic trick!")
+    
+#TRY IT YOURSELF
+#PIZAAAA
+pizzas = ['caprichoza', 'camen', 'margarita']
+
+for pizza in pizzas:
+    print(f"{pizza.title()} is the best pizza in our restaurant!")
+    print(f"I think pepperoni is better than {pizza.title()}!\n")
+
+#ANIMALS
+animals  = ['monkey','hamster','shark']
+for i,animal in enumerate(animals,1):
+    print(f'{i}.{animal} is going to be a great pet')
+print("Any of this animals would make a great pet!")
